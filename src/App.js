5ad3efbb3049a0fff12f137e9d10084cf72b1832
Shivproj <shivindrakanti@gmail.com>
2022-11-29 19:38:15 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import A from "./Components/29112022/A.js";
 import B from "./Components/29112022/B.js";
+import Table from "./Components/28112022/Table.js";
 import{  BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App ">      
-      
+    <div className="App ">  
+    <Table/>    
+{/*       
       <A/>
-      <B/>
+      <B/> */}
     </div>
     </Router>
   );
