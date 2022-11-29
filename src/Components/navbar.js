d490e02313a2f267afb1eb7a5f299a4154b0af90
Shivproj <shivindrakanti@gmail.com>
2022-11-29 18:51:23 +0530

    small change

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -2,14 +2,14 @@ const Navbar =()=>{
     return(
     <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">First-App</a>
+      <a class="navbar-brand" >First-App</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" >Home</a>
           </li>         
          </ul>   
        
