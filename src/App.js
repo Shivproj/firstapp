07610eedff50fd5799f4d2a69f0329892cbce01c
Shivproj <shivindrakanti@gmail.com>
2022-11-30 14:32:23 +0530

    reverting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import Table from "./Components/28112022/Table";
 
 function App() {
   return (
   
     <div className="App "> 
-    <Table/>
-    <h1>Made with love by <a href="https://github.com/Shivproj/firstapp/tree/main/src/Components/28112022">Shiv Sai</a></h1>
-    
+ 
+
+
       
       
       
